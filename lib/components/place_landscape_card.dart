24a10719b4b76737b589e24e8cbccc686d2a0b66
Maxim <Maxzb86@gmail.com>
2024-10-24 00:29:46 +0300

    Discs: add like heart

--- lib/components/place_landscape_card.dart
+++ lib/components/place_landscape_card.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 import '../models/place.dart';
 
-class PlaceLandscapeCard extends StatelessWidget {
+class PlaceLandscapeCard extends StatefulWidget {
   final Place place;
   const PlaceLandscapeCard({
     super.key,
     required this.place,
   });
+
+  @override
+  State<PlaceLandscapeCard> createState() => _PlaceLandscapeCardState();
+}
+
+class _PlaceLandscapeCardState extends State<PlaceLandscapeCard> {
+  bool _isFavorited = false;
 
   @override
   Widget build(BuildContext context) {
@@ -18,34 +25,49 @@ class PlaceLandscapeCard extends StatelessWidget {
         mainAxisSize: MainAxisSize.min,
         children: [
           ClipRRect(
-            // 1
-            borderRadius: const BorderRadius.vertical(
-              top: Radius.circular(8.0),
-            ),
-            // 2
-            child: AspectRatio(
-              aspectRatio: 2,
-              child: Image.asset(
-                place.imageUrl,
-                fit: BoxFit.cover,
+              borderRadius: const BorderRadius.vertical(
+                top: Radius.circular(8.0),
               ),
-            ),
-          ),
+              child: AspectRatio(
+                aspectRatio: 2,
+                child: Stack(
+                  fit: StackFit.expand,
+                  children: [
+                    Image.asset(
+                      widget.place.imageUrl,
+                      fit: BoxFit.cover,
+                    ),
+                    Positioned(
+                      top: 4.0,
+                      right: 4.0,
+                      child: IconButton(
+                        icon: Icon(
+                          _isFavorited ? Icons.favorite : Icons.favorite_border,
+                        ),
+                        iconSize: 30.0,
+                        color: Colors.red[400],
+                        onPressed: () {
+                          setState(() {
+                            _isFavorited = !_isFavorited;
+                          });
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              )),
           ListTile(
-            // 1
             title: Text(
-              place.name,
+              widget.place.name,
               style: textTheme.titleSmall,
             ),
-            // 2
             subtitle: Text(
-              place.attributes,
+              widget.place.attributes,
               maxLines: 1,
               style: textTheme.bodySmall,
             ),
-            // 3
             onTap: () {
-              // ignore: avoid_print   print('Tap on ${restaurant.name}');
+              // ignore: avoid_print
             },
           ),
         ],
